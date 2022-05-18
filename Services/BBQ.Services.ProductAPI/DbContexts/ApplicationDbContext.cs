ce0dd8d50d209c951d25aa7b44f17ea7b536c506
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-05-17 19:30:19 -0500

    set up database

---
+++ Services/BBQ.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BBQ.Services.ProductAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BBQ.Services.ProductAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
