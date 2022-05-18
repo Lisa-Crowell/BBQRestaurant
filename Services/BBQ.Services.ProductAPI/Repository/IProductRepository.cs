33124f29ebf8b8fa384750be8587dccc086c42e1
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-05-18 17:03:49 -0500

    create IProductRepository.cs

--- Services/BBQ.Services.ProductAPI/Repository/IProductRepository.cs
+++ Services/BBQ.Services.ProductAPI/Repository/IProductRepository.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BBQ.Services.ProductAPI.Models.Dto;
 
 namespace BBQ.Services.ProductAPI.Repository
 {
     public interface IProductRepository
     {
-        
+        Task<IEnumerable<ProductDto>> GetProducts();
+        Task<ProductDto> GetProductById(int productId);
+        // method to create or update a product  (ProductModel)
+        Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+        // method to delete product using bool value
+        Task<bool> DeleteProduct(int productId);
     }
 }
