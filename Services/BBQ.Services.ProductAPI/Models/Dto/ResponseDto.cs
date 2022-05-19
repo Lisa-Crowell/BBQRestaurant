6db1e1ddbc9d454c5f268c8775b17c10af81c528
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-05-19 14:58:21 -0500

    correct typo

--- Services/BBQ.Services.ProductAPI/Models/Dto/ResponseDto.cs
+++ Services/BBQ.Services.ProductAPI/Models/Dto/ResponseDto.cs
@@ -5,5 +5,5 @@ public class ResponseDto
     public bool IsSuccess { get; set; } = true;
     public object Result { get; set; }
     public string DisplayMessage { get; set; } = "";
-    public List<string> ErrorMessage { get; set; }
+    public List<string> ErrorMessages { get; set; }
 }
