6580a1794ca9e650a981cd48a0bb83f58db86e45
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-05-18 20:47:07 -0500

    create MappingConfig.cs

---
+++ Services/BBQ.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using BBQ.Services.ProductAPI.Models;
+using BBQ.Services.ProductAPI.Models.Dto;
+
+namespace BBQ.Services.ProductAPI
+{
+
+    public class MappingConfig
+    {
+        // method that returns auto config for AutoMapper
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDto, Product>();
+                config.CreateMap<Product, ProductDto>();
+            });
+            return mappingConfig;
+        }
+    }
+}
