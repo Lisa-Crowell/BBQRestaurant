fee8076af748bfb6a0675d85118794263ba1a29a
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-05-19 17:37:07 -0500

    setup database/Azure with dummy data

--- Services/BBQ.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
+++ Services/BBQ.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -10,4 +10,47 @@ public class ApplicationDbContext : DbContext
     }
 
     public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 1,
+                Name = "BBQ Sliders",
+                Price = 13.99,
+                Description = "Your choice of 100% Angus beef or chicken sliders with homemade barbeque sauce on a Hawaiian sweetbread bun.<br/>Choose from Sweet and Tangy, Hot and Spicy, or Honey BBQ sauces with your choice of toppings.",
+                ImageUrl = "https://lisadotnetmastery.blob.core.windows.net/bbq/ImagesForBBQ/sliders.jpg",
+                // public static string? GetEnvironmentVariable (string varstring);
+                CategoryName = "Appetizer"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 2,
+                Name = "Sweet Potato Fries",
+                Price = 9.99,
+                Description = "What BBQ is complete without a side of Southern Sweet Potato Fries? <br/>Hand breaded and fried to perfection. Choose from Tangy Ranch, Dill BBQ sauce, or Honey Mustard to dip them in. Your mouth will thank you!",
+                ImageUrl = "https://lisadotnetmastery.blob.core.windows.net/bbq/ImagesForBBQ/sweetpotatofries.jpg",
+                CategoryName = "Appetizer"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 3,
+                Name = "Apple Pie",
+                Price = 10.99,
+                Description = "A flaky crust just like Mama used to make. Enjoy a slice this handed down family recipe, <br/>from our family to ours.",
+                ImageUrl = "https://lisadotnetmastery.blob.core.windows.net/bbq/ImagesForBBQ/applepie.jpg",
+                CategoryName = "Dessert"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 4,
+                Name = "Beef Brisket",
+                Price = 15.99,
+                Description = "Cooked to perfection this beef brisket has been smoking for hours waiting to be enjoyed! <br/>Smoked over hickory and maple chips for a flavor that cannot be beat!",
+                ImageUrl = "https://lisadotnetmastery.blob.core.windows.net/bbq/ImagesForBBQ/beefbrisket.jpg",
+                CategoryName = "Entree"
+            });
+    }
 }
