b53c41ea8cd8b8638271bc1ef2ca568e73c1ffce
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-05-19 18:57:10 -0500

    create ApiRequest.cs and IBaseService.cs update ApiRequest.cs

--- FrontEnd/BBQ.Web/Models/ApiRequest.cs
+++ FrontEnd/BBQ.Web/Models/ApiRequest.cs
@@ -1,6 +1,11 @@
-﻿namespace BBQ.Web.Models;
+﻿using static BBQ.Web.SD;
+
+namespace BBQ.Web.Models;
 
 public class ApiRequest
 {
-    
+    public ApiType ApiType { get; set; } = ApiType.GET;
+    public string Url { get; set; }
+    public object Data { get; set; }
+    public string AccessToken { get; set; }
 }
---
+++ FrontEnd/BBQ.Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,6 @@
+﻿namespace BBQ.Web.Services.IServices;
+
+public class IBaseService
+{
+    
+}
