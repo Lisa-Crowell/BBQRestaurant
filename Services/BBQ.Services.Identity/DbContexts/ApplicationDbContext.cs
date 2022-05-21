663b08f8bdbea4ceef0de37267d8bc24a54435b6
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-05-21 15:15:42 -0500

    update ApplicationDbContext.cs

--- Services/BBQ.Services.Identity/DbContexts/ApplicationDbContext.cs
+++ Services/BBQ.Services.Identity/DbContexts/ApplicationDbContext.cs
@@ -1,6 +1,13 @@
-﻿namespace BBQ.Services.Identity.DbContexts;
+﻿using BBQ.Services.Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
-public class ApplicationDbContext
+namespace BBQ.Services.Identity.DbContexts;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
-    
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
 }
