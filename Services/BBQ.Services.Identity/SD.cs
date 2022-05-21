639452414f8da4fb2d2b470521961a0db25a3e15
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-05-21 15:18:36 -0500

    update SD.cs

---
+++ Services/BBQ.Services.Identity/SD.cs
@@ -0,0 +1,59 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+
+namespace BBQ.Services.Identity;
+
+public static class SD
+{
+    public const string Admin = "Admin";
+    public const string Customer = "Customer";
+
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new List<IdentityResource>
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Email(),
+            new IdentityResources.Profile()
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new List<ApiScope>
+        {
+            new("bbq", "BBQ Server"),
+            new("read", "Read your data."),
+            new("write", "Write your data."),
+            new("delete", "Delete your data.")
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new List<Client>
+        {
+            new()
+            {
+                ClientId = "client",
+                ClientSecrets= { new Secret("secret".Sha256())},
+                // ClientSecrets = Environment.GetEnvironmentVariable("CLIENT_SECRETS"),
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                AllowedScopes = {"read", "write", "profile"}
+            },
+         
+            new()
+            {
+                ClientId = "bbq",
+                ClientSecrets= { new Secret("secret".Sha256())},
+                // ClientSecrets = Environment.GetEnvironmentVariable("CLIENT_SECRETS"),
+                AllowedGrantTypes = GrantTypes.Code,
+                RedirectUris={ "https://localhost:44378/signin-oidc" },
+                PostLogoutRedirectUris={"https://localhost:44378/signout-callback-oidc" },
+                // RedirectUris = Environment.GetEnvironmentVariable("REDIRECT_URIS"),
+                // PostLogoutRedirectUris = Environment.GetEnvironmentVariable("POST_LOGOUT_REDIRECT_URIS"),
+                AllowedScopes = new List<string>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    IdentityServerConstants.StandardScopes.Email,
+                    "bbq"
+                }
+            }
+        };
+}
