9aa6f43df42041a3da9d233df77d2d44cff37e0e
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-05-21 22:49:06 -0500

    correct typo

--- FrontEnd/BBQ.Web/Controllers/HomeController.cs
+++ FrontEnd/BBQ.Web/Controllers/HomeController.cs
@@ -38,6 +38,6 @@ public class HomeController : Controller
 
     public IActionResult Logout()
     {
-        return SignOut("Cookies", "iodc");
+        return SignOut("Cookies", "oidc");
     }
 }
