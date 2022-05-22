8031f1ebe8a588230bcb8b5f911e7baee5fe9a7a
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-05-22 18:26:13 -0500

    reformat files

--- Services/BBQ.Services.Identity/MainModule/Account/RegisterViewModel.cs
+++ Services/BBQ.Services.Identity/MainModule/Account/RegisterViewModel.cs
@@ -1,37 +1,33 @@
-﻿using IdentityServerHost.Quickstart.UI;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using IdentityServerHost.Quickstart.UI;
 
-namespace BBQ.Services.Identity.MainModule.Account
+namespace BBQ.Services.Identity.MainModule.Account;
+
+public class RegisterViewModel
 {
-    public class RegisterViewModel
-    {
-        [Required]
-        public string Username { get; set; }
+    [Required] public string Username { get; set; }
 
-        [Required]
-        public string Email { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        [Required]
-        public string Password { get; set; }
+    [Required] public string Email { get; set; }
 
-        public string ReturnUrl { get; set; }
-        public string RoleName { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
 
-        public bool AllowRememberLogin { get; set; } = true;
-        public bool EnableLocalLogin { get; set; } = true;
+    [Required] public string Password { get; set; }
 
-        public IEnumerable<ExternalProvider> ExternalProviders { get; set; } = Enumerable.Empty<ExternalProvider>();
-        public IEnumerable<ExternalProvider> VisibleExternalProviders => ExternalProviders.Where(x => !String.IsNullOrWhiteSpace(x.DisplayName));
+    public string ReturnUrl { get; set; }
+    public string RoleName { get; set; }
 
-        public bool IsExternalLoginOnly => EnableLocalLogin == false && ExternalProviders?.Count() == 1;
-        // public string ExternalLoginScheme => IsExternalLoginOnly ? ExternalProviders?.SingleOrDefault()?.AuthenticationScheme : null;
-        public string? ExternalLoginScheme =>
-            IsExternalLoginOnly ? ExternalProviders?.SingleOrDefault()?.AuthenticationScheme : null;
+    public bool AllowRememberLogin { get; set; } = true;
+    public bool EnableLocalLogin { get; set; } = true;
 
-    }
+    public IEnumerable<ExternalProvider> ExternalProviders { get; set; } = Enumerable.Empty<ExternalProvider>();
+
+    public IEnumerable<ExternalProvider> VisibleExternalProviders =>
+        ExternalProviders.Where(x => !string.IsNullOrWhiteSpace(x.DisplayName));
+
+    public bool IsExternalLoginOnly => EnableLocalLogin == false && ExternalProviders?.Count() == 1;
+
+    // public string ExternalLoginScheme => IsExternalLoginOnly ? ExternalProviders?.SingleOrDefault()?.AuthenticationScheme : null;
+    public string? ExternalLoginScheme =>
+        IsExternalLoginOnly ? ExternalProviders?.SingleOrDefault()?.AuthenticationScheme : null;
 }
