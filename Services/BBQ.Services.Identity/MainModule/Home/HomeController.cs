ce99cdd64f6d0b5a6198a803b0217c6375b689e5
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-05-22 18:25:21 -0500

    reformat files

--- Services/BBQ.Services.Identity/MainModule/Home/HomeController.cs
+++ Services/BBQ.Services.Identity/MainModule/Home/HomeController.cs
@@ -2,64 +2,56 @@
 // See LICENSE in the project root for license information.
 
 
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System.Threading.Tasks;
 using Duende.IdentityServer.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
-namespace IdentityServerHost.Quickstart.UI
+namespace IdentityServerHost.Quickstart.UI;
+
+[SecurityHeaders]
+[AllowAnonymous]
+public class HomeController : Controller
 {
-    [SecurityHeaders]
-    [AllowAnonymous]
-    public class HomeController : Controller
+    private readonly IWebHostEnvironment _environment;
+    private readonly IIdentityServerInteractionService _interaction;
+    private readonly ILogger _logger;
+
+    public HomeController(IIdentityServerInteractionService interaction, IWebHostEnvironment environment,
+        ILogger<HomeController> logger)
     {
-        private readonly IIdentityServerInteractionService _interaction;
-        private readonly IWebHostEnvironment _environment;
-        private readonly ILogger _logger;
+        _interaction = interaction;
+        _environment = environment;
+        _logger = logger;
+    }
 
-        public HomeController(IIdentityServerInteractionService interaction, IWebHostEnvironment environment, ILogger<HomeController> logger)
-        {
-            _interaction = interaction;
-            _environment = environment;
-            _logger = logger;
-        }
+    public IActionResult Index()
+    {
+        if (_environment.IsDevelopment())
+            // only show in development
+            return View();
 
-        public IActionResult Index()
+        _logger.LogInformation("Homepage is disabled in production. Returning 404.");
+        return NotFound();
+    }
+
+    /// <summary>
+    ///     Shows the error page
+    /// </summary>
+    public async Task<IActionResult> Error(string errorId)
+    {
+        var vm = new ErrorViewModel();
+
+        // retrieve error details from identityServer
+        var message = await _interaction.GetErrorContextAsync(errorId);
+        if (message != null)
         {
-            if (_environment.IsDevelopment())
-            {
+            vm.Error = message;
+
+            if (!_environment.IsDevelopment())
                 // only show in development
-                return View();
-            }
-
-            _logger.LogInformation("Homepage is disabled in production. Returning 404.");
-            return NotFound();
+                message.ErrorDescription = null;
         }
 
-        /// <summary>
-        /// Shows the error page
-        /// </summary>
-        public async Task<IActionResult> Error(string errorId)
-        {
-            var vm = new ErrorViewModel();
-
-            // retrieve error details from identityServer
-            var message = await _interaction.GetErrorContextAsync(errorId);
-            if (message != null)
-            {
-                vm.Error = message;
-
-                if (!_environment.IsDevelopment())
-                {
-                    // only show in development
-                    message.ErrorDescription = null;
-                }
-            }
-
-            return View("Error", vm);
-        }
+        return View("Error", vm);
     }
 }
