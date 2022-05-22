48e7d3ff4187c75fba78695bec77c3d722957d0d
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-05-22 18:28:07 -0500

    reformat files

--- Services/BBQ.Services.Identity/MainModule/Device/DeviceAuthorizationInputModel.cs
+++ Services/BBQ.Services.Identity/MainModule/Device/DeviceAuthorizationInputModel.cs
@@ -2,10 +2,9 @@
 // See LICENSE in the project root for license information.
 
 
-namespace IdentityServerHost.Quickstart.UI
+namespace IdentityServerHost.Quickstart.UI;
+
+public class DeviceAuthorizationInputModel : ConsentInputModel
 {
-    public class DeviceAuthorizationInputModel : ConsentInputModel
-    {
-        public string UserCode { get; set; }
-    }
+    public string UserCode { get; set; }
 }
--- Services/BBQ.Services.Identity/MainModule/Device/DeviceAuthorizationViewModel.cs
+++ Services/BBQ.Services.Identity/MainModule/Device/DeviceAuthorizationViewModel.cs
@@ -2,11 +2,10 @@
 // See LICENSE in the project root for license information.
 
 
-namespace IdentityServerHost.Quickstart.UI
+namespace IdentityServerHost.Quickstart.UI;
+
+public class DeviceAuthorizationViewModel : ConsentViewModel
 {
-    public class DeviceAuthorizationViewModel : ConsentViewModel
-    {
-        public string UserCode { get; set; }
-        public bool ConfirmUserCode { get; set; }
-    }
+    public string UserCode { get; set; }
+    public bool ConfirmUserCode { get; set; }
 }
