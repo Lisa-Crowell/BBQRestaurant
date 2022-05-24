728bbee79fe86c06e4bfe5c1c533699631f0aff1
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-05-24 01:57:09 -0500

    implement CreateUpdateCart

---
+++ Services/BBQ.Services.ShoppingCartAPI/Repository/CartRepository.cs
@@ -0,0 +1,86 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using ShoppingCartAPI.DbContexts;
+using ShoppingCartAPI.Models;
+using ShoppingCartAPI.Models.Dto;
+
+namespace BBQ.Services.ShoppingCartAPI.Repository;
+
+public class CartRepository : ICartRepository
+{
+    private readonly ApplicationDbContext _db;
+    private readonly IMapper _mapper;
+
+    public CartRepository(ApplicationDbContext db, IMapper mapper)
+    {
+        _db = db;
+        _mapper = mapper;
+    }
+    
+    public async Task<CartDto> GetCartByUserId(string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<CartDto> CreateUpdateCart(CartDto cartDto)
+    {
+        Cart cart = _mapper.Map<Cart>(cartDto);
+        // check if product exists in database, if not then create it
+        var prodInDb = await _db.Products.FirstOrDefaultAsync(u => 
+            u.ProductId == cartDto.CartDetails.FirstOrDefault().ProductId);
+        if (prodInDb == null)
+        {
+            _db.Products.Add(cart.CartDetails.FirstOrDefault().Product);
+            await _db.SaveChangesAsync();
+        }
+        // check if header is null
+        var cartHeaderFromDb = await _db.CartHeaders.AsNoTracking().FirstOrDefaultAsync(u => 
+            u.UserId == cart.CartHeader.UserId);
+        if (cartHeaderFromDb == null)
+        {
+            // create header and details
+            _db.CartHeaders.Add(cart.CartHeader);
+            await _db.SaveChangesAsync();
+            cart.CartDetails.FirstOrDefault().CartHeaderId = cart.CartHeader.CartHeaderId;
+            cart.CartDetails.FirstOrDefault().Product = null;
+            _db.CartDetails.Add(cart.CartDetails.FirstOrDefault());
+            await _db.SaveChangesAsync();
+        }
+        else
+        {
+             // if header is not null check if details has same product
+             var cartDetailsFromDb = await _db.CartDetails.AsNoTracking().FirstOrDefaultAsync(u =>
+                 u.ProductId == cart.CartDetails.FirstOrDefault().ProductId &&
+                 u.CartHeaderId == cartHeaderFromDb.CartHeaderId);
+
+             if (cartDetailsFromDb == null)
+             {
+                 // create details
+                 cart.CartDetails.FirstOrDefault().CartHeaderId = cartHeaderFromDb.CartHeaderId;
+                 cart.CartDetails.FirstOrDefault().Product = null;
+                 _db.CartDetails.Add(cart.CartDetails.FirstOrDefault());
+                 await _db.SaveChangesAsync();
+             }
+             else
+             {
+                 // update the count / cart details
+                 cart.CartDetails.FirstOrDefault().Product = null;
+                 cart.CartDetails.FirstOrDefault().Count += cartDetailsFromDb.Count;
+                 _db.CartDetails.Update(cart.CartDetails.FirstOrDefault());
+                 await _db.SaveChangesAsync();
+             }
+        }
+
+        return _mapper.Map<CartDto>(cartDto);
+    }
+
+    public async Task<bool> RemoveFromCart(int cartDetailsId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> ClearCart(string userId)
+    {
+        throw new NotImplementedException();
+    }
+}
