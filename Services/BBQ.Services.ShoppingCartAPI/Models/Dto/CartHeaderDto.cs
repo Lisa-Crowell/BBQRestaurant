05d473fd4c54ae48a8be6faa1264f92fe78e487a
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-05-24 17:57:26 -0500

    code cleanup and fix errors

---
+++ Services/BBQ.Services.ShoppingCartAPI/Models/Dto/CartHeaderDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BBQ.Services.ShoppingCartAPI.Models.Dto;
+
+public class CartHeaderDto
+{
+    public int CartHeaderId { get; set; }
+    public string UserId { get; set; }
+    public string CouponCode { get; set; }
+}
