2adda64b8fd0078711b49f0d38a0018d265cad00
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-05-24 17:01:12 -0500

    initials commit shopping cart

---
+++ Services/BBQ.Services.ShoppingCart/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BBQ.Services.ShoppingCart.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
