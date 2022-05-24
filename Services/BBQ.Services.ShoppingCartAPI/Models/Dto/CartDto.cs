00819d58fa725015f7f0d80da89bd78903ada7cb
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-05-23 23:23:08 -0500

    create CartDto.cs

---
+++ Services/BBQ.Services.ShoppingCartAPI/Models/Dto/CartDto.cs
@@ -0,0 +1,9 @@
+﻿using BBQ.Services.ShoppingCartAPI.Models;
+
+namespace ShoppingCartAPI.Models.Dto;
+
+public class CartDto
+{
+    public CartHeader CartHeader { get; set; }
+    public IEnumerable<CartDetails> CartDetails { get; set; }
+}
