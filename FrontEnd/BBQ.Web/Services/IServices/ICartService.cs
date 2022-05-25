416ac2d33956c168e2160d6880d9f650bbe9cf1d
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-05-24 19:36:05 -0500

    correct error

--- FrontEnd/BBQ.Web/Services/IServices/ICartService.cs
+++ FrontEnd/BBQ.Web/Services/IServices/ICartService.cs
@@ -4,8 +4,8 @@ namespace BBQ.Web.Services.IServices;
 
 public interface ICartService
 {
-    Task<T> GetCartByUserIdAsync<T>(string userId, string token = null);
-    Task<T> AddToCartAsync<T>(CartDto cartDto, string token = null);
-    Task<T> UpdateCartAsync<T>(CartDto cartDto, string token = null);
-    Task<T> RemoveFromCartAsync<T>(int cartId, string token = null);
+    Task<T> GetCartByUserIdAsync<T>(string userId, string accessToken = null);
+    Task<T> AddToCartAsync<T>(CartDto cartDto, string accessToken = null);
+    Task<T> UpdateCartAsync<T>(CartDto cartDto, string accessToken = null);
+    Task<T> RemoveFromCartAsync<T>(int cartId, string accessToken = null);
 }
