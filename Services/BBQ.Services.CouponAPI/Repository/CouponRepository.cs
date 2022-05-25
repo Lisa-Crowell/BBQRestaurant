c75864c7a7c84818f099ad8114f46df868a72a86
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-05-25 16:09:48 -0500

    create CouponRepository.cs

---
+++ Services/BBQ.Services.CouponAPI/Repository/CouponRepository.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using BBQ.Services.CouponAPI.DbContexts;
+using BBQ.Services.CouponAPI.Models.Dto;
+
+namespace BBQ.Services.CouponAPI.Repository;
+
+public class CouponRepository : ICouponRepository
+{
+    private readonly ApplicationDbContext _db;
+    protected IMapper _mapper;
+    public CouponRepository(ApplicationDbContext db, IMapper mapper)
+    {
+        _db = db;
+        _mapper = mapper;
+    }
+    public Task<CouponDto> GetCouponByCode(string couponCode)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Services/BBQ.Services.CouponAPI/Repository/ICouponRepository.cs
@@ -0,0 +1,8 @@
+﻿using BBQ.Services.CouponAPI.Models.Dto;
+
+namespace BBQ.Services.CouponAPI.Repository;
+
+public interface ICouponRepository
+{
+    Task<CouponDto> GetCouponByCode(string couponCode);
+}
