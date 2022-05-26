d57ed76e5456ebfd8dfa8a114970c4bb76acf312
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-05-26 01:20:38 -0500

    implement CouponService.cs & ICouponService.cs

---
+++ FrontEnd/BBQ.Web/Services/IServices/CouponService.cs
@@ -0,0 +1,22 @@
+﻿using BBQ.Web.Models;
+
+namespace BBQ.Web.Services.IServices;
+
+public class CouponService : BaseService, ICouponService
+{
+    private readonly IHttpClientFactory _clientFactory;
+
+    public CouponService(IHttpClientFactory clientFactory) : base(clientFactory)
+    {
+        _clientFactory = clientFactory;
+    }
+    public async Task<T> GetCoupon<T>(string couponCode, string accessToken = null)
+    {
+        return await this.SendAsync<T>(new ApiRequest()
+        {
+            ApiType = SD.ApiType.GET,
+            Url = SD.CouponAPIBase + "/api/coupon/" + couponCode,
+            AccessToken = accessToken
+        });
+    }
+}
