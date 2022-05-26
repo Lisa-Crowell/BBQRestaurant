5eba6ba579e77817273e246b8554f67215b82253
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-05-26 01:35:10 -0500

    update CartHeaderDto.cs

--- FrontEnd/BBQ.Web/Models/CartHeaderDto.cs
+++ FrontEnd/BBQ.Web/Models/CartHeaderDto.cs
@@ -9,7 +9,7 @@
         public double DiscountTotal { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public DateTime PickupDate { get; set; }
+        public DateTime PickupDateTime { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
         public string CardNumber { get; set; }
