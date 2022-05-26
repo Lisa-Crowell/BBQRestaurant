c0da326ca18a596b3fc420a4b69ca90cae1f2c11
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-05-26 00:34:44 -0500

    update with apply/remove coupon

--- Services/BBQ.Services.ShoppingCartAPI/Controllers/CartAPIControllers.cs
+++ Services/BBQ.Services.ShoppingCartAPI/Controllers/CartAPIControllers.cs
@@ -85,4 +85,37 @@ public class CartAPIController : Controller
 
         return _response;
     }
+    [HttpPost("ApplyCoupon")]
+    public async Task<object> ApplyCoupon([FromBody] CartDto cartDto)
+    {
+        try
+        {
+            var isSuccess = await _cartRepository.ApplyCoupon(cartDto.CartHeader.UserId,
+                cartDto.CartHeader.CouponCode);
+            _response.Result = isSuccess;
+        }
+        catch (Exception ex)
+        {
+            _response.IsSuccess = false;
+            _response.ErrorMessages = new List<string> {ex.ToString()};
+        }
+
+        return _response;
+    }
+    [HttpPost("RemoveCoupon")]
+    public async Task<object> RemoveCoupon([FromBody] string userId)
+    {
+        try
+        {
+            bool isSuccess = await _cartRepository.RemoveCoupon(userId);
+            _response.Result = isSuccess;
+        }
+        catch (Exception ex)
+        {
+            _response.IsSuccess = false;
+            _response.ErrorMessages = new List<string> {ex.ToString()};
+        }
+
+        return _response;
+    }
 }
