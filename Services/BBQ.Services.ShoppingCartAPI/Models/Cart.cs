36a3bca9032557b4317d63b86083dc327bb390d6
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-05-27 14:00:30 -0500

    remove typo

--- Services/BBQ.Services.ShoppingCartAPI/Models/Cart.cs
+++ Services/BBQ.Services.ShoppingCartAPI/Models/Cart.cs
@@ -2,6 +2,6 @@
 
 public class Cart
 {
-    public CartHeader? CartHeader { get; set; }
+    public CartHeader CartHeader { get; set; }
     public IEnumerable<CartDetails> CartDetails { get; set; }
 }
