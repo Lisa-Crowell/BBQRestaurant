5c9c01bd316aa72c85d8ce6e6776f184e001b85f
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-05-26 22:41:10 -0500

    correct error

--- Services/BBQ.Services.ShoppingCartAPI/Repository/ICartRepository.cs
+++ Services/BBQ.Services.ShoppingCartAPI/Repository/ICartRepository.cs
@@ -4,8 +4,8 @@ namespace BBQ.Services.ShoppingCartAPI.Repository;
 
 public interface ICartRepository
 {
-    Task<Cart> GetCartByUserId(string userId);
-    Task<Cart> CreateUpdateCart(CartDto cartDto);
+    Task<CartDto> GetCartByUserId(string userId);
+    Task<CartDto> CreateUpdateCart(CartDto cartDto);
     Task<bool> RemoveFromCart(int cartDetailsId);
     Task<bool> ApplyCoupon(string userId, string couponCode);
     Task<bool> RemoveCoupon(string userId);
