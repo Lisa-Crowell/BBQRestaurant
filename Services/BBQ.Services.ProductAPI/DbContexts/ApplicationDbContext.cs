d6f83efeec8d17c7424f1dc73ca26b939b89bed6
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-05-28 15:58:49 -0500

    update environment variables

--- Services/BBQ.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
+++ Services/BBQ.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -22,7 +22,7 @@ public class ApplicationDbContext : DbContext
             Price = 13.99,
             Description =
                 "Your choice of 100% Angus beef or chicken sliders with homemade barbeque sauce on a Hawaiian sweetbread bun. Choose from Sweet and Tangy, Hot and Spicy, or Honey BBQ sauces with your choice of toppings.",
-            ImageUrl = "https://lisadotnetmastery.blob.core.windows.net/bbq/ImagesForBBQ/sliders2.jpg",
+            ImageUrl = Environment.GetEnvironmentVariable("IMAGE_URL_PIE"),
             CategoryName = "Appetizer"
         });
         modelBuilder.Entity<Product>().HasData(new Product
@@ -32,7 +32,7 @@ public class ApplicationDbContext : DbContext
             Price = 9.99,
             Description =
                 "What BBQ is complete without a side of Southern Sweet Potato Fries? Hand breaded and fried to perfection. Choose from Tangy Ranch, Dill BBQ sauce, or Honey Mustard to dip them in. Your mouth will thank you!",
-            ImageUrl = "https://lisadotnetmastery.blob.core.windows.net/bbq/ImagesForBBQ/sweetpotatofries2.jpg",
+            ImageUrl = Environment.GetEnvironmentVariable("IMAGE_URL_FRIES"),
             CategoryName = "Appetizer"
         });
         modelBuilder.Entity<Product>().HasData(new Product
@@ -42,7 +42,7 @@ public class ApplicationDbContext : DbContext
             Price = 10.99,
             Description =
                 "A buttery, fall apart in your mouth delicious, flaky crust just like Grandma used to make with sweet and tart apple filling. Enjoy a slice of this multi-generationally handed down family recipe, from our family to ours!",
-            ImageUrl = "https://lisadotnetmastery.blob.core.windows.net/bbq/ImagesForBBQ/applepie2.jpg",
+            ImageUrl = Environment.GetEnvironmentVariable("IMAGE_URL_PIE"),
             CategoryName = "Dessert"
         });
         modelBuilder.Entity<Product>().HasData(new Product
@@ -52,7 +52,7 @@ public class ApplicationDbContext : DbContext
             Price = 15.99,
             Description =
                 "Cooked to perfection, this beef brisket has been smoking for hours waiting to be enjoyed! <br/>Smoked over a blend of hickory and maple chips for a flavor that cannot be beat!",
-            ImageUrl = "https://lisadotnetmastery.blob.core.windows.net/bbq/ImagesForBBQ/beefbrisket.jpg",
+            ImageUrl = Environment.GetEnvironmentVariable("IMAGE_URL_BRISKET"),
             CategoryName = "Entree"
         });
     }
