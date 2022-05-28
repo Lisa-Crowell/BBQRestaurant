eb2eac6b2ae4fca4f61ae8cf7d460f7080a06daf
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-05-28 13:57:10 -0500

    add CheckoutHeaderDto.cs

--- Services/BBQ.Services.ShoppingCartAPI/Controllers/CartAPIControllers.cs
+++ Services/BBQ.Services.ShoppingCartAPI/Controllers/CartAPIControllers.cs
@@ -1,4 +1,5 @@
-﻿using BBQ.Services.ShoppingCartAPI.Models.Dto;
+﻿using BBQ.Services.ShoppingCartAPI.Messages;
+using BBQ.Services.ShoppingCartAPI.Models.Dto;
 using BBQ.Services.ShoppingCartAPI.Repository;
 using Microsoft.AspNetCore.Mvc;
 
@@ -113,6 +114,29 @@ public class CartAPIController : Controller
         {
             var isSuccess = await _cartRepository.RemoveCoupon(userId);
             _response.Result = isSuccess;
+        }
+        catch (Exception ex)
+        {
+            _response.IsSuccess = false;
+            _response.ErrorMessages = new List<string> {ex.ToString()};
+        }
+
+        return _response;
+    }
+    
+    [HttpPost("Checkout")]
+    public async Task<object> Checkout(CheckoutHeaderDto checkoutHeader)
+    {
+        try
+        {
+            CartDto cartDto = await _cartRepository.GetCartByUserId(checkoutHeader.UserId);
+            if (cartDto == null)
+            {
+                return BadRequest();
+            }
+
+            checkoutHeader.CartDetails = cartDto.CartDetails;
+            // logic to add message to process order
         }
         catch (Exception ex)
         {
