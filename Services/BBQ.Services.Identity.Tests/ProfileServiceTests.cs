8a3bc1a4ddd88151eac687727abb6ff4e18363eb
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-05-30 12:33:07 -0500

    create test using NUnit testing

---
+++ Services/BBQ.Services.Identity.Tests/ProfileServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using BBQ.Services.Identity.Models;
+using Duende.IdentityServer.Services;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+using NUnit.Framework;
+
+namespace BBQ.Services.Identity;
+
+[TestFixture]
+public class ProfileServiceTests
+{
+    private Mock<IProfileService> _profileServiceMock;
+    private Mock<ApplicationUser> _userMgr;
+    private Mock<IdentityRole> _roleMgr;
+    private Mock<IUserClaimsPrincipalFactory<ApplicationUser>> userClaimsPrincipalFactory;
+    private bool x;
+
+    [SetUp]
+    public void Setup()
+    {
+        _profileServiceMock = new Mock<IProfileService>();
+
+        x = true;
+    }
+
+    [Test]
+    public void TestOne()
+    {
+        Assert.That(x == true);
+    }
+}
