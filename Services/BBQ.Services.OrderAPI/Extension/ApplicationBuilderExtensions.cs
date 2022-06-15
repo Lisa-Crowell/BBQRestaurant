38e238e6129f7b4d69bfa90aedd3baad5b5818bf
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-06-15 00:21:18 -0500

    create ApplicationBuilderExtensions.cs

---
+++ Services/BBQ.Services.OrderAPI/Extension/ApplicationBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using BBQ.Services.OrderAPI.Messaging;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BBQ.Services.OrderAPI.Extension;
+
+public static class ApplicationBuilderExtensions
+{
+    public static IAzureServiceBusConsumer ServiceBusConsumer { get; set; }
+
+    public static IApplicationBuilder UseAzureServiceBusConsumer(this IApplicationBuilder app)
+    {
+        ServiceBusConsumer = app.ApplicationServices.GetRequiredService<AzureServiceBusConsumer>();
+        var hostApplicationLife = app.ApplicationServices.GetService<IHostApplicationLifetime>();
+
+        hostApplicationLife.ApplicationStarted.Register(OnStart);
+        hostApplicationLife.ApplicationStopped.Register(OnStop);
+        return app;
+    }
+
+    private static void OnStart()
+    {
+        ServiceBusConsumer.Start();
+    }
+
+    private static void OnStop()
+    {
+        ServiceBusConsumer.Stop();
+    }
+}
