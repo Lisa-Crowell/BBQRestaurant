cfe859ba1b1df654b610b2c5f6b1319963bb05f7
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-06-15 00:25:28 -0500

    create Order Repository and Interface

---
+++ Services/BBQ.Services.OrderAPI/Repository/IOrderRepository.cs
@@ -0,0 +1,9 @@
+﻿using BBQ.Services.OrderAPI.Models;
+
+namespace BBQ.Services.OrderAPI.Repository;
+
+public interface IOrderRepository
+{
+    Task<bool> AddOrder(OrderHeader orderHeader);
+    Task UpdateOrderPaymentStatus(int orderHeaderId, bool paid);
+}
