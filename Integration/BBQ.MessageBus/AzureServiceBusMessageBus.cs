5087c722ba847e15a4f667105761816181154f89
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-06-15 17:24:23 -0500

    add environment variable

---
+++ Integration/BBQ.MessageBus/AzureServiceBusMessageBus.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+
+namespace BBQ.MessageBus;
+
+public class AzureServiceBusMessageBus : IMessageBus
+{
+    private string connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+    
+    public async Task PublishMessage(BaseMessage message, string topicName)
+    {
+        await using var client = new ServiceBusClient(connectionString);
+
+        ServiceBusSender sender = client.CreateSender(topicName);
+        
+        var jMessage = JsonConvert.SerializeObject(message);
+        ServiceBusMessage finalMessage = new ServiceBusMessage(Encoding.UTF8.GetBytes(jMessage))
+        {
+            CorrelationId = Guid.NewGuid().ToString()
+        };
+        await sender.SendMessageAsync(finalMessage);
+        await client.DisposeAsync();
+    }
+}
