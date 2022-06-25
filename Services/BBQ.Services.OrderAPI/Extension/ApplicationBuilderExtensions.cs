e0024e5465e127cdde0a0ac2bf7af97a9a8ea415
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-06-25 00:25:46 -0500

    correct error

--- Services/BBQ.Services.OrderAPI/Extension/ApplicationBuilderExtensions.cs
+++ Services/BBQ.Services.OrderAPI/Extension/ApplicationBuilderExtensions.cs
@@ -1,24 +1,21 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Hosting;
-using BBQ.Services.OrderAPI.Messaging;
-using Microsoft.Extensions.DependencyInjection;
+﻿using BBQ.Services.OrderAPI.Messaging;
+
 
 namespace BBQ.Services.OrderAPI.Extension;
 
 public static class ApplicationBuilderExtensions
 {
-    public static IAzureServiceBusConsumer ServiceBusConsumer { get; set; }
 
+    public static IAzureServiceBusConsumer ServiceBusConsumer { get; set; }
     public static IApplicationBuilder UseAzureServiceBusConsumer(this IApplicationBuilder app)
     {
-        ServiceBusConsumer = app.ApplicationServices.GetRequiredService<AzureServiceBusConsumer>();
+        ServiceBusConsumer = app.ApplicationServices.GetService<IAzureServiceBusConsumer>();
         var hostApplicationLife = app.ApplicationServices.GetService<IHostApplicationLifetime>();
 
         hostApplicationLife.ApplicationStarted.Register(OnStart);
         hostApplicationLife.ApplicationStopped.Register(OnStop);
         return app;
     }
-
     private static void OnStart()
     {
         ServiceBusConsumer.Start();
