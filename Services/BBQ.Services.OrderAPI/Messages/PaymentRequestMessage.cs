32487fb9106b0d23a7bad0d10f27ee03acc1d43d
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-06-25 14:23:27 -0500

    implement PaymentRequestMessage.cs

--- Services/BBQ.Services.OrderAPI/Messages/PaymentRequestMessage.cs
+++ Services/BBQ.Services.OrderAPI/Messages/PaymentRequestMessage.cs
@@ -1,6 +1,13 @@
-﻿namespace BBQ.Services.OrderAPI.Messages;
+﻿using BBQ.MessageBus;
 
-public class PaymentRequestMessage
+namespace BBQ.Services.OrderAPI.Messages;
+
+public class PaymentRequestMessage : BaseMessage
 {
-    
+    public int OrderId { get; set; }
+    public string Name { get; set; }
+    public string CardNumber { get; set; }
+    public string CVV { get; set; }
+    public string ExpiryMonthYear { get; set; }
+    public double OrderTotal { get; set; }
 }
