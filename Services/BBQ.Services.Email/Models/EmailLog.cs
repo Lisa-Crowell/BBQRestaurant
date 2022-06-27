23501ee93ffba7e7c0aeddcb7cec13646212baf6
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-06-27 14:36:27 -0500

    create EmailLog.cs

---
+++ Services/BBQ.Services.Email/Models/EmailLog.cs
@@ -0,0 +1,9 @@
+﻿namespace BBQ.Services.Email.Models;
+
+public class EmailLog
+{
+    public int Id { get; set; }
+    public string Email { get; set; }
+    public string Log { get; set; }
+    public DateTime EmailSent { get; set; }
+}
