d9fb5bd77b971002dbdfe0fcd634a4fafe8ad880
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-02 15:54:49 -0500

    update AzureServiceBusConsumer.cs

--- Services/BBQ.Services.Email/Messaging/AzureServiceBusConsumer.cs
+++ Services/BBQ.Services.Email/Messaging/AzureServiceBusConsumer.cs
@@ -8,66 +8,56 @@ namespace BBQ.Services.Email.Messaging;
 
 public class AzureServiceBusConsumer : IAzureServiceBusConsumer
 {
+    private readonly IConfiguration _configuration;
+
+    private readonly EmailRepository _emailRepo;
+    private readonly string orderUpdatePaymentResultTopic;
     private readonly string serviceBusConnectionString;
-        private readonly string subscriptionEmail;
-        private readonly string orderUpdatePaymentResultTopic;
+    private readonly string subscriptionEmail;
 
-        private readonly EmailRepository _emailRepo;
+    private ServiceBusProcessor orderUpdatePaymentStatusProcessor;
 
-        private ServiceBusProcessor orderUpdatePaymentStatusProcessor;
+    public AzureServiceBusConsumer(EmailRepository emailRepo, IConfiguration configuration)
+    {
+        _emailRepo = emailRepo;
+        _configuration = configuration;
 
-        private readonly IConfiguration _configuration;
-
-        public AzureServiceBusConsumer(EmailRepository emailRepo, IConfiguration configuration)
-        {
-            _emailRepo = emailRepo;
-            _configuration = configuration;
-
-            serviceBusConnectionString = _configuration.GetValue<string>("ServiceBusConnectionString");
-            subscriptionEmail = _configuration.GetValue<string>("SubscriptionName");
-            orderUpdatePaymentResultTopic = _configuration.GetValue<string>("OrderUpdatePaymentResultTopic");
+        serviceBusConnectionString = _configuration.GetValue<string>("ServiceBusConnectionString");
+        subscriptionEmail = _configuration.GetValue<string>("SubscriptionName");
+        orderUpdatePaymentResultTopic = _configuration.GetValue<string>("OrderUpdatePaymentResultTopic");
 
 
-            var client = new ServiceBusClient(serviceBusConnectionString);
+        var client = new ServiceBusClient(serviceBusConnectionString);
 
-            orderUpdatePaymentStatusProcessor = client.CreateProcessor(orderUpdatePaymentResultTopic, subscriptionEmail);
-        }
+        orderUpdatePaymentStatusProcessor = client.CreateProcessor(orderUpdatePaymentResultTopic, subscriptionEmail);
+    }
 
-        public async Task Start()
-        {
-          
-            orderUpdatePaymentStatusProcessor.ProcessMessageAsync += OnOrderPaymentUpdateReceived;
-            orderUpdatePaymentStatusProcessor.ProcessErrorAsync += ErrorHandler;
-            await orderUpdatePaymentStatusProcessor.StartProcessingAsync();
-        }
-        public async Task Stop()
-        {
-            
-            await orderUpdatePaymentStatusProcessor.StopProcessingAsync();
-            await orderUpdatePaymentStatusProcessor.DisposeAsync();
-        }
-        Task ErrorHandler(ProcessErrorEventArgs args)
-        {
-            Console.WriteLine(args.Exception.ToString());
-            return Task.CompletedTask;
-        }
+    public async Task Start()
+    {
+        orderUpdatePaymentStatusProcessor.ProcessMessageAsync += OnOrderPaymentUpdateReceived;
+        orderUpdatePaymentStatusProcessor.ProcessErrorAsync += ErrorHandler;
+        await orderUpdatePaymentStatusProcessor.StartProcessingAsync();
+    }
 
-  
+    public async Task Stop()
+    {
+        await orderUpdatePaymentStatusProcessor.StopProcessingAsync();
+        await orderUpdatePaymentStatusProcessor.DisposeAsync();
+    }
 
-        private async Task OnOrderPaymentUpdateReceived(ProcessMessageEventArgs args)
-        {
-            var message = args.Message;
-            var body = Encoding.UTF8.GetString(message.Body);
+    private Task ErrorHandler(ProcessErrorEventArgs args)
+    {
+        Console.WriteLine(args.Exception.ToString());
+        return Task.CompletedTask;
+    }
+    
+    private async Task OnOrderPaymentUpdateReceived(ProcessMessageEventArgs args)
+    {
+        var message = args.Message;
+        var body = Encoding.UTF8.GetString(message.Body);
 
-            UpdatePaymentResultMessage objMessage = JsonConvert.DeserializeObject<UpdatePaymentResultMessage>(body);
-            try
-            {
-                await _emailRepo.SendAndLogEmail(objMessage);
-                await args.CompleteMessageAsync(args.Message);
-            }
-            catch (Exception e)
-            {
-                throw;
-            }
-        }
+        var objMessage = JsonConvert.DeserializeObject<UpdatePaymentResultMessage>(body);
+        await _emailRepo.SendAndLogEmail(objMessage);
+        await args.CompleteMessageAsync(args.Message);
+    }
 }
