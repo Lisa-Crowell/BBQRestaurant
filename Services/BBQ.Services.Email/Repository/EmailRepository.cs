c94e0677f8f49588591db985dfaf24b70489c036
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-03 12:21:56 -0500

    implement EmailRepository.cs

--- Services/BBQ.Services.Email/Repository/EmailRepository.cs
+++ Services/BBQ.Services.Email/Repository/EmailRepository.cs
@@ -1,6 +1,30 @@
-﻿namespace BBQ.Services.Email.Repository;
+﻿using BBQ.Services.Email.DbContexts;
+using BBQ.Services.Email.Messages;
+using BBQ.Services.Email.Models;
+using Microsoft.EntityFrameworkCore;
 
-public class EmailRepository
+namespace BBQ.Services.Email.Repository;
+
+public class EmailRepository : IEmailRepository
 {
-    
+    private readonly DbContextOptions<ApplicationDbContext> _dbContext;
+
+    public EmailRepository(DbContextOptions<ApplicationDbContext> dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task SendAndLogEmail(UpdatePaymentResultMessage message)
+    {
+       
+        EmailLog emailLog = new EmailLog()
+        {
+            Email = message.Email,
+            EmailSent = DateTime.Now,
+            Log = $"Order - {message.OrderId} has been successfully created."
+        };
+        await using var _db = new ApplicationDbContext(_dbContext);
+        _db.EmailLogs.Add(emailLog);
+        await _db.SaveChangesAsync();
+    }
 }
