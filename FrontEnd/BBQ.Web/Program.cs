51145df526d9b66d129abf6618bcdfb777e9e511
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-08-09 16:14:50 -0500

    error corrections

--- FrontEnd/BBQ.Web/Program.cs
+++ FrontEnd/BBQ.Web/Program.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.AddEnvironmentVariables();
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient<IProductService, ProductService>();
